e88f84458fcbf758b2130b79ad029b5198e399de
Sergey_Miroshkin <miroshkin.mirus@ya.ru>
2019-01-19 13:39:27 +0300

    Выполнил задание№1 уровня - easy

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -4,13 +4,38 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
+
 def my_round(number, ndigits):
-    pass
+    parts = str(number).split(".")
+    characters = [int(x) for x in reversed(parts[1])]
 
+    answers = []
+    transfer = 0
+    steps = len(characters) - ndigits
+    step = 0
 
-print(my_round(2.1234567, 5))
-print(my_round(2.1999967, 5))
-print(my_round(2.9999967, 5))
+    for character in characters:
+
+        character += transfer
+
+        if character == 10:
+            answers.append("0")
+            transfer = 1
+        else:
+            answers.append(str(character))
+            transfer = 0
+
+        if step < steps and character > 4:
+            transfer = 1
+            step += 1
+
+    before = int(parts[0]) + transfer
+    after = "".join(reversed(answers))
+
+    if ndigits == 0:
+        return before
+    else:
+        return float(str(before) + "." + after[:ndigits])
 
 
 # Задание-2:
@@ -23,6 +48,10 @@ def lucky_ticket(ticket_number):
     pass
 
 
-print(lucky_ticket(123006))
-print(lucky_ticket(12321))
-print(lucky_ticket(436751))
+#print(lucky_ticket(123006))
+#print(lucky_ticket(12321))
+#print(lucky_ticket(436751))
+
+print(my_round(2.1234567, 5))
+print(my_round(2.1999967, 5))
+print(my_round(2.9999967, 5))
