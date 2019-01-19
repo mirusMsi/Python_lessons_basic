a21a16f371f6a44cbb84d2e88d5da8888fc6a15c
Sergey_Miroshkin <miroshkin.mirus@ya.ru>
2019-01-19 14:41:02 +0300

    Выполнил задание№1 уровня - normal

--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -2,8 +2,23 @@
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
+
 def fibonacci(n, m):
-    pass
+
+    fibonacci = []
+    answers = []
+
+    def fibonacci_count(before, number, amount, array):
+        array.append(number)
+        if amount > 1:
+            fibonacci_count(number, before + number, amount - 1, array)
+
+    fibonacci_count(0, 1, m, fibonacci)
+
+    for x in range(n - 1, m):
+        answers.append(fibonacci[x])
+
+    return answers
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
@@ -14,7 +29,7 @@ def fibonacci(n, m):
 def sort_to_max(origin_list):
     pass
 
-sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
+#sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
@@ -25,3 +40,4 @@ sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
 
+print(fibonacci(5, 12))
