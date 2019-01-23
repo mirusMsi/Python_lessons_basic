a93772ddfa90ee05de1145fd92172d876bad008f
Sergey_Miroshkin <miroshkin.mirus@ya.ru>
2019-01-24 00:27:37 +0300

    home work

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,8 +1,14 @@
+import re
+import random
+import os
+
 # Задание-1:
 # Вывести символы в нижнем регистре, которые находятся вокруг
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
+
+# я возможно не так понял задание, но по суте необходимо вытащить полные строчные последовательности
 
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
@@ -19,6 +25,24 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+
+def task_1(variable):
+    answer = []
+    string = ""
+
+    for i in variable:
+        if 64 < ord(i) < 91:
+            if string != "":
+                answer.append(string)
+                string = ""
+        else:
+            string = string + i
+
+    if string != "":
+        answer.append(string)
+
+    return answer
 
 
 # Задание-2:
@@ -45,9 +69,81 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+
+def search(variable):
+
+    top = list(map(lambda x: chr(x), list(range(65, 91))))
+    bottom = list(map(lambda x: chr(x), list(range(97, 123))))
+
+    lst = list(variable)
+    lst.extend(["", "", "", ""])
+    length = len(lst)
+
+    check = ""
+    answer = []
+
+    for i in range(length - 4):
+        x = lst[i]
+        if lst[i] in bottom and lst[i + 1] in bottom and lst[i + 2] in top and lst[i + 3] in top and lst[i + 4] in top:
+            check = check + lst[i]
+        elif lst[i] in bottom and lst[i - 1] in bottom and lst[i + 1] in top and lst[i + 2] in top and lst[i + 3] in top:
+            check = check + lst[i]
+        elif len(check) > 1:
+            if lst[i] in top and lst[i + 1] in top:
+                check = check + lst[i]
+            else:
+                answer.append(check[2:-1])
+                check = ""
+
+    return answer
+
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+
+def max_ran(maximum):
+
+    lst = [random.randint(0, 9) for i in range(maximum)]
+    string = ''.join(list(map(lambda x: str(x), lst)))
+
+    way = os.path.join("data", "text.txt")
+
+    with open(way, 'w', encoding='UTF-8') as file:
+        file.write(str(string))
+
+    with open(way, 'r', encoding='UTF-8') as file:
+        string_new = list(file.read())
+
+    string_new.extend([""])
+    length = len(string_new)
+
+    answer = []
+    check = []
+    for i in range(length - 1):
+        if string_new[i] == string_new[i + 1]:
+            check.append(string_new[i])
+        elif string_new[i] == string_new[i - 1] and i > 0:
+            check.append(string_new[i])
+            if len(check) > len(answer):
+                answer.clear()
+                answer.extend(check)
+                check.clear()
+            else:
+                check.clear()
+
+    return string_new, answer
+
+
+print(task_1(line))
+print(re.findall(r"[a-z]+", line))
+
+print(search(line_2))
+print(re.findall(r"[a-z]{2}([A-Z]+)[A-Z]{2}", line_2))
+
+a, b = max_ran(2500)
+print(a, b, sep="\n")
